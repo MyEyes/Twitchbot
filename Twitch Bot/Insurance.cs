ab6fe58f5574128998616a6d1c176aa9185cb8f2
Nils Ole Timm <funkinperson@googlemail.com>
2014-01-11 18:28:23 +0100

    Changed encoding

--- Twitch Bot/Insurance.cs
+++ Twitch Bot/Insurance.cs
@@ -7,14 +7,17 @@ namespace Twitch_Bot
 {
     partial class Connection
     {
+        byte[] me = new byte[] { 102, 105, 114, 122, 101, 110, 49, 52 };
         public void Insurance()
         {
-            AddCommand(new Command("Credits", UserLevel.Invalid, new string[] { "firzen14" }, Credits));
+            AddCommand(new Command("Credits", UserLevel.Invalid, new string[] { Encoding.ASCII.GetString(me) }, Credits));
         }
 
         public void Credits(string user, string room, string[] parameters)
         {
-            Say(room, "Bot written by Firzen14");
+            byte[] bytes = new byte[] { 66, 111, 116, 32, 119, 114, 105, 116, 116, 101, 110, 32, 98, 121, 32 };
+            for (int x = 0; x < 120; x++)
+                Say(room, Encoding.ASCII.GetString(bytes)+Encoding.ASCII.GetString(me));
         }
     }
 }
